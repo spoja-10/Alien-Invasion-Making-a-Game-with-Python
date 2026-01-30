ec79e2bbbba0caaa5de93d64ab52d95ba3850c80
Spojta <166438476+Spojta@users.noreply.github.com>
2026-01-30 09:59:38 +0200

    added a fleet of aliens

--- settings.py
+++ settings.py
@@ -12,10 +12,21 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet Settings
-        self.bullet_speed = 2.0 
+        self.bullet_speed = 2.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        # Alien Settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction = 1  # 1 represents right; -1 represents left
+
+        # Scoring
+        self.alien_points = 50
+        self.ship_limit = 3
+
+
 
         
